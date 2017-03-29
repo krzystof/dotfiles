fa790b5837880c711090ff9c6e835ce86ca0d4c0
Christophe Graniczny <Christophe.Graniczny@avantiplc.net>
2017-03-29 10:49:12 +0100

    I do need the sync script

---
+++ sync.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -eu
+
+# Update the dotfiles repo
+cd $HOME/Dotfiles
+git pull
+cd ~
+
+# Delete some directories
+function remove_music_if_exists() {
+    DIR_TO_RM=$HOME/Music/$1
+    if [[ -e "${DIR_TO_RM}" ]]; then
+        rm -rf "${DIR_TO_RM}"
+    fi
+}
+
+remove_music_if_exists "Von Bondies"
+
